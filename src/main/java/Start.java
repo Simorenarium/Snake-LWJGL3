1d16add7089c45d86838abebb1892684443c395a
BuildTools <unconfigured@null.spigotmc.org>
2021-09-17 16:42:19 +0200

    ✨ Skidded own OSType enum

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,4 +1,26 @@
+import utils.OSType;
+
 public final class Start {
+    static {
+        final String architecture = System.getProperty("os.arch");
+        final String opSystem = System.getProperty("os.name").toLowerCase();
+        final boolean isArm64 = architecture.contains("arm64");
+        final boolean isAmd64 = architecture.contains("amd64");
+        final boolean isX64 = architecture.contains("x64");
+        final boolean isOverX86 = (isArm64 || isAmd64 || isX64);
+        final OSType osType;
+        if (opSystem.contains("win"))
+            osType = isOverX86 ? OSType.WINDOWS_64x : OSType.WINDOWS_32x;
+        else if (opSystem.contains("linux") || opSystem.contains("unix"))
+            osType = isX64 ? OSType.LINUX_x64 : isArm64 ? OSType.LINUX_arm64 : OSType.LINUX_arm32;
+        else if (opSystem.contains("mac") || opSystem.contains("sunos") || opSystem.contains("solaris")
+                || opSystem.contains("darwin"))
+            osType = OSType.MACOS_x64;
+        else osType = OSType.LINUX_arm32;
+        OSType.setCurrentOS(osType);
+    }
+
     public static void main(final String... args) {
+        System.out.println(OSType.getCurrentOS());
     }
 }
---
+++ src/main/java/utils/OSType.java
@@ -0,0 +1,24 @@
+package utils;
+
+public enum OSType {
+
+    WINDOWS_32x("windows", "x86"),
+    WINDOWS_64x("windows", "x64"),
+    LINUX_arm32("linux", "arm32"),
+    LINUX_arm64("linux", "arm64"),
+    LINUX_x64("linux", "x64"),
+    MACOS_x64("macos", "x64");
+
+    private static OSType currentOS = null;
+
+    OSType(final String name, final String arch) {}
+
+    public static OSType getCurrentOS() {
+        return currentOS;
+    }
+
+    public static void setCurrentOS(final OSType osType) {
+        currentOS = osType;
+    }
+
+}
